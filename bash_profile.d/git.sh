f9c4a013b996eb70153f4db3b1ae95203567a413
Elijah Miller <elijah.miller@gmail.com>
2008-12-23 18:48:23 -0500

    Adding grc alias for git rebase --continue

--- bash_profile.d/git.sh
+++ bash_profile.d/git.sh
@@ -6,6 +6,7 @@ alias gc='git commit -v'
 alias gca='gc -a'
 alias gp='git pull --rebase'
 alias gpp='gp && git push'
+alias grc='git rebase --continue'
 
 current_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
