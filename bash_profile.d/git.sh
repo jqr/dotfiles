09ac576446efc145ea59b521d567a61eb9183daf
Elijah Miller <elijah.miller@gmail.com>
2009-02-03 09:13:48 -0500

    Adding autocomplete for gb alias, from http://richardhulse.blogspot.com/2008/06/using-git.html

--- bash_profile.d/git.sh
+++ bash_profile.d/git.sh
@@ -11,6 +11,7 @@ alias gpp='gp && git push origin `current_git_branch`'
 alias grc='git rebase --continue'
 
 alias gb='git branch'
+complete -o default -o nospace -F _git_branch gb
 
 current_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
