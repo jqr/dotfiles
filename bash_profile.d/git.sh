26da4ddf55b837e4c3a49e2faaf7dea08870e8f5
Elijah Miller <elijah.miller@gmail.com>
2009-02-03 09:06:58 -0500

    Adding gb alias for git branch.

--- bash_profile.d/git.sh
+++ bash_profile.d/git.sh
@@ -1,12 +1,16 @@
 alias gs='git status'
 alias gd='git diff'
 alias gdh='git diff HEAD'
+
 alias ga='git add'
 alias gc='git commit -v'
 alias gca='gc -a'
+
 alias gp='git pull --rebase || (which growlnotify > /dev/null && growlnotify -m "pull failed" "Git")'
 alias gpp='gp && git push origin `current_git_branch`'
 alias grc='git rebase --continue'
+
+alias gb='git branch'
 
 current_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
