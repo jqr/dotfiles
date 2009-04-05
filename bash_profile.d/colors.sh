27a2e21aaaa5938e2fabb9cd320227320903d8bf
Elijah Miller <elijah.miller@gmail.com>
2009-04-05 13:01:06 -0400

    Adding standard constant names for colors.

---
+++ bash_profile.d/colors.sh
@@ -0,0 +1,19 @@
+# http://www.novell.com/coolsolutions/tools/17142.html
+
+BLACK='\e[0;30m'
+BLUE='\e[0;34m'
+GREEN='\e[0;32m'
+CYAN='\e[0;36m'
+RED='\e[0;31m'
+PURPLE='\e[0;35m'
+BROWN='\e[0;33m'
+LIGHTGRAY='\e[0;37m'
+DARKGRAY='\e[1;30m'
+LIGHTBLUE='\e[1;34m'
+LIGHTGREEN='\e[1;32m'
+LIGHTCYAN='\e[1;36m'
+LIGHTRED='\e[1;31m'
+LIGHTPURPLE='\e[1;35m'
+YELLOW='\e[1;33m'
+WHITE='\e[1;37m'
+NC='\e[0m'              # No Color
