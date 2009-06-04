ddcfb03b8ab961c84cb6b3802f5856e88161e145
Elijah Miller <elijah.miller@gmail.com>
2009-06-04 17:06:12 -0400

    Added sql_bench for super simplistic benchmarking and explaining of raw SQL.

--- railsrc.d/sql.rb
+++ railsrc.d/sql.rb
@@ -3,3 +3,12 @@
 def sql(query)
   ActiveRecord::Base.connection.select_all(query)
 end
+
+def sql_bench(query, runs = 10)
+  puts
+  pp sql("EXPLAIN #{query}").first
+  puts
+  puts "#{runs} runs in"
+  runs_time = Benchmark.realtime { runs.times { sql(query) } }
+  puts "  #{runs_time}"
+end
