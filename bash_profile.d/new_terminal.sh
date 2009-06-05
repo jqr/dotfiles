1dab3f64aef5e03c514c762535df7c350946adbd
Elijah Miller <elijah.miller@gmail.com>
2009-06-05 18:43:35 -0400

    Adding the nt alias for new terminal/tab.

---
+++ bash_profile.d/new_terminal.sh
@@ -0,0 +1,11 @@
+# http://gist.github.com/124422
+
+function nt {
+  terminal_clone_command="
+tell application \"Terminal\"
+tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down
+do script with command \"cd `pwd`; clear\" in selected tab of the front window
+end tell
+"
+  echo "$terminal_clone_command" | osascript &>/dev/null
+}
