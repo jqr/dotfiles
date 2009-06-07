6cb65643f2b2eb2e7ba00db2b1d5563e5df42501
Elijah Miller <elijah.miller@gmail.com>
2009-06-07 10:42:52 -0400

    Removing unnecessary clear command.

--- bash_profile.d/new_terminal.sh
+++ bash_profile.d/new_terminal.sh
@@ -4,7 +4,7 @@ function nt {
   terminal_clone_command="
 tell application \"Terminal\"
 tell application \"System Events\" to tell process \"Terminal\" to keystroke \"t\" using command down
-do script with command \"cd `pwd`; clear\" in selected tab of the front window
+do script with command \"cd `pwd`\" in selected tab of the front window
 end tell
 "
   echo "$terminal_clone_command" | osascript &>/dev/null
