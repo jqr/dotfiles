d207626e6974e8116d8f127ed0930b0d3c246421
Elijah Miller <elijah.miller@gmail.com>
2009-06-17 19:49:29 -0400

    Fixing local_methods to handle being called on a class or instance.

--- irbrc.d/local_methods.rb
+++ irbrc.d/local_methods.rb
@@ -1,6 +1,14 @@
 # From http://github.com/ryanb/dotfiles/tree/master/irbrc
+# improved to handle instance and class methods
 
-# list methods which aren't in superclass
-def local_methods(obj = self)
-  (obj.methods - obj.class.superclass.instance_methods).sort
+class Object
+  # List class methods which aren't in the superclass.
+  def self.local_methods
+    (methods - superclass.methods).sort
+  end
+  
+  # List instance methods which aren't in the superclass.
+  def local_methods
+    (methods - self.class.superclass.instance_methods).sort
+  end
 end
