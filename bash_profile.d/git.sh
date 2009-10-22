0b3b3e6e7591e203a36657e175da35775767d29b
Elijah Miller <elijah.miller@gmail.com>
2009-10-22 10:12:07 -0400

    Adding a simple alias for git pull/push that also checks that rake passes.

--- bash_profile.d/git.sh
+++ bash_profile.d/git.sh
@@ -16,6 +16,7 @@ alias gca='gc -a'
 
 alias gp='git pull --rebase || (notify "pull failed" "Git" && false)'
 alias gpp='gp && git push origin `current_git_branch`'
+alias gprp='gp && rake && gpp'
 alias gri='git rebase -i origin/master^'
 alias grc='git rebase --continue'
 
