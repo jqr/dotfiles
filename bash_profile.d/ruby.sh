d0ed668d4511c73708143b3469e3b0dcbb0c1124
Elijah Miller <elijah.miller@gmail.com>
2010-05-21 18:46:24 -0400

    Removing incompatible gme commands.

--- bash_profile.d/ruby.sh
+++ bash_profile.d/ruby.sh
@@ -1,27 +1,5 @@
 # Inspiration from http://github.com/hashrocket/dotmatrix/blob/master/.hashrc
 
-function gemdir {
-  if [ $1 ]
-    then
-    ls -d `gemdir`/$1* | sort | tail -1
-  else
-    echo `gem env gemdir`/gems
-  fi
-}
-
-function cdgem {
-  cd `gemdir $1`
-}
-
-function gemdoc {
-  open `gemdir $1`/doc/index.html ||
-  open `gemdir $1`/README*
-}
-function editgem {
-  $EDITOR `gemdir $1`
-}
-
-
 function rails_root {
   (
     dir=${1:-$(pwd)}
