782a42a508b7e99157b51b2b97585d6e7cb04496
Elijah Miller <elijah.miller@gmail.com>
2010-07-09 17:42:16 -0400

    Added pman (Preview man) and ql (Quick Look) aliases.

--- bash_profile.d/mac.sh
+++ bash_profile.d/mac.sh
@@ -1,2 +1,9 @@
 alias m="mate"
 alias openports='sudo lsof -iTCP -sTCP:LISTEN -P'
+
+# http://superuser.com/questions/52483/terminal-tips-and-tricks-for-mac-os-x
+alias ql='qlmanage -p 2>/dev/null'
+
+pman () {
+    man -t "${1}" | open -f -a /Applications/Preview.app
+}
