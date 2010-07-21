ecedd965926014e24c8067049f670188d641dd29
Elijah Miller <elijah.miller@gmail.com>
2010-07-20 22:57:03 -0400

    Ruby history script works on Ruby 1.9+.

--- irbrc.d/history.rb
+++ irbrc.d/history.rb
@@ -5,14 +5,14 @@ begin
   histfile = File::expand_path(HISTFILE)
   if File::exists?(histfile)
     lines = IO::readlines(histfile).collect { |line| line.chomp }
-    puts "Read #{lines.nitems} saved history commands from '#{histfile}'." if $VERBOSE
+    puts "Read #{lines.size} saved history commands from '#{histfile}'." if $VERBOSE
     Readline::HISTORY.push(*lines)
   else
     puts "History file '#{histfile}' was empty or non-existant." if $VERBOSE
   end
   Kernel::at_exit do
     lines = Readline::HISTORY.to_a.reverse.uniq.reverse
-    lines = lines[-MAXHISTSIZE, MAXHISTSIZE] if lines.nitems > MAXHISTSIZE
+    lines = lines[-MAXHISTSIZE, MAXHISTSIZE] if lines.size > MAXHISTSIZE
     puts "Saving #{lines.length} history lines to '#{histfile}'." if $VERBOSE
     File::open(histfile, File::WRONLY|File::CREAT|File::TRUNC) { |io| io.puts lines.join("\n") }
   end
