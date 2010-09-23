a104f8c3b2ce66db22ec3a0cc3bfee3ce00c4fbb
Elijah Miller <elijah.miller@gmail.com>
2010-09-23 09:49:55 -0400

    Detecting SSH session and displaying username@host in red.

--- bash_profile.d/prompt.sh
+++ bash_profile.d/prompt.sh
@@ -1,1 +1,5 @@
-PS1="$GREEN\h $RED\w $LIGHTRED\$(current_git_branch)\$(git_modifications)$LIGHTRED$ $NC"
+if [ $SSH_TTY ]; then
+  PS1="$RED\u@\h $RED\w $LIGHTRED\$(current_git_branch)\$(git_modifications)$LIGHTRED$ $NC"
+else
+  PS1="$GREEN\h $RED\w $LIGHTRED\$(current_git_branch)\$(git_modifications)$LIGHTRED$ $NC"
+fi
