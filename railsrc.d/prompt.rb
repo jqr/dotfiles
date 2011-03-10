97012f16d1a6c06e3356eccafff0c731e6e13cfc
Elijah Miller <elijah.miller@gmail.com>
2011-03-10 02:07:12 -0500

    Showing environment name in Rails console.

--- railsrc.d/prompt.rb
+++ railsrc.d/prompt.rb
@@ -1,1 +1,9 @@
-setup_custom_irb_prompt(File.basename(DETECTED_RAILS_ROOT))
+def setup_rails_irb_prompt
+  dangerous_envs = %w(production)
+  rails_env = ENV['RAILS_ENV']
+  rails_env = rails_env.upcase if dangerous_envs.include?(rails_env)
+    
+  setup_custom_irb_prompt(File.basename(DETECTED_RAILS_ROOT) + " #{rails_env}")
+end
+
+setup_rails_irb_prompt
