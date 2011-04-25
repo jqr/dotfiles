73e63e0b202496b0c24cea754a0b26b9081ae4e9
Elijah Miller <elijah.miller@gmail.com>
2011-04-25 12:51:58 -0400

    Proper handling of more than 5 arguments, also simpler code.

--- bash_profile.d/first_of.sh
+++ bash_profile.d/first_of.sh
@@ -3,7 +3,9 @@
 #  EDITOR=`first_of "mate -w" "nano -w" vi`
 first_of() {
   if [ -n "$1" ]; then
-    command -v $1 >> /dev/null && echo $1 || first_of "$2" "$3" "$4" "$5"
+    local arg=$1
+    shift
+    command -v $arg >> /dev/null && echo $arg || first_of "$@"
   else
     exit 1
   fi
