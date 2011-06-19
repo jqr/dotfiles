5fa4125648dde1fb324fa0b3a721e7f24a803dcb
Elijah Miller <elijah.miller@gmail.com>
2011-06-18 20:46:38 -0500

    Not showing error if ~/.ssh/known_hosts doesn't exist.

--- bash_profile.d/standard_improvements.sh
+++ bash_profile.d/standard_improvements.sh
@@ -20,6 +20,6 @@ myip() {
 _ssh_hosts() {
   grep "Host " ~/.ssh/config 2> /dev/null | sed -e "s/Host //g"
   # http://news.ycombinator.com/item?id=751220
-  cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["
+  cat ~/.ssh/known_hosts 2> /dev/null | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["
 }
 complete -W "$(_ssh_hosts)" ssh
