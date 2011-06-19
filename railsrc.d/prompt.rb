f1ada158a45cd1dbeee04a0cb7f49bcb963dbce9
Elijah Miller <elijah.miller@gmail.com>
2011-06-18 21:34:43 -0500

    Graceful handing of rails root not being detectable.

--- railsrc.d/prompt.rb
+++ railsrc.d/prompt.rb
@@ -2,8 +2,9 @@ def setup_rails_irb_prompt
   dangerous_envs = %w(production)
   rails_env = ENV['RAILS_ENV']
   rails_env = rails_env.upcase if dangerous_envs.include?(rails_env)
-    
-  setup_custom_irb_prompt(File.basename(DETECTED_RAILS_ROOT) + " #{rails_env}")
+
+  prompt = defined?(DETECTED_RAILS_ROOT) ? File.basename(DETECTED_RAILS_ROOT) : 'rails'
+  setup_custom_irb_prompt(prompt + " #{rails_env}")
 end
 
 setup_rails_irb_prompt
