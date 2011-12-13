d825c23580f67de54e8c7a7ceb2de35e9d07a46e
Elijah Miller <elijah.miller@gmail.com>
2011-12-12 21:40:06 -0500

    Handling no awesome_print in a nicer way.

--- irbrc.d/awesome_print.rb
+++ irbrc.d/awesome_print.rb
@@ -1,8 +1,11 @@
-require 'ap'
+begin
+  require 'ap'
 
-IRB::Irb.class_eval do
-  def output_value
-    ap @context.last_value
+  IRB::Irb.class_eval do
+    def output_value
+      ap @context.last_value
+    end
   end
+rescue LoadError
+  # no awesome print for you
 end
-
