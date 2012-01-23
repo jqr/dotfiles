2ad5420723e6c4a4012b435c8f346cb65d45aaa4
Elijah Miller <elijah.miller@gmail.com>
2012-01-22 20:50:13 -0500

    Probably a bad sign that I need this.

--- bash_profile.d/standard_aliases.sh
+++ bash_profile.d/standard_aliases.sh
@@ -2,6 +2,7 @@ alias reload='source ~/.bash_profile && echo "Reloaded ~/.bash_profile"'
 
 alias ..='cd ..'
 alias ...='cd ../..'
+alias ....='cd ../../..'
 alias cd..='cd ..'
 alias ll='ls -lh'
 alias la='ls -lA'
