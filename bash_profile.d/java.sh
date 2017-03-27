442b19fbee062b9e289bbc8f77e66fef2bc942b5
Elijah Miller <jqr@meek.local>
2017-03-27 09:22:07 -0400

    Handle java-less systems without erroring.

--- bash_profile.d/java.sh
+++ bash_profile.d/java.sh
@@ -1,2 +1,10 @@
-export JAVA_HOME
-JAVA_HOME=$(/usr/libexec/java_home)
+find_java_home() {
+  local java_home
+  java_home=$(/usr/libexec/java_home 2> /dev/null)
+  if [[ "$java_home" ]]; then
+    export JAVA_HOME
+    JAVA_HOME=$java_home
+  fi
+}
+
+find_java_home
