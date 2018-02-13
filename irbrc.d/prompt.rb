b1fdb4c130879004302a0b1cb637936d72c4dd41
Elijah Miller <elijah.miller@gmail.com>
2018-02-13 15:01:09 -0500

    Support for default prompt setup in Pry too.

--- irbrc.d/prompt.rb
+++ irbrc.d/prompt.rb
@@ -1,26 +1,31 @@
-def setup_custom_irb_prompt(name)
-  return unless IRB.respond_to?(:conf) && IRB.conf[:PROMPT]
+def setup_custom_repl_prompt(name)
   green = "\e[32m"
   red = "\e[31m"
   clear = "\e[0m"
 
-  IRB.conf[:PROMPT][:CUSTOM] = {
-    # Flag  Description
-    # %N    Current command.
-    # %m    to_s of the main object (self).
-    # %M    inspect of the main object (self).
-    # %l    Delimiter type. In strings that are continued across a line break, %l will display the type of delimiter used to begin the string, so you'll know how to end it. The delimiter will be one of ", ', /, ], or `.
-    # %ni   Indent level. The optional number n is used as a width specification to printf, as printf("%nd").
-    # %nn   Current line number (n used as with the indent level).
-    # %%    A literal percent sign.
+  if defined?(IRB) && IRB.respond_to?(:conf) && IRB.conf[:PROMPT]
+    IRB.conf[:PROMPT][:CUSTOM] = {
+      # Flag  Description
+      # %N    Current command.
+      # %m    to_s of the main object (self).
+      # %M    inspect of the main object (self).
+      # %l    Delimiter type. In strings that are continued across a line break, %l will display the type of delimiter used to begin the string, so you'll know how to end it. The delimiter will be one of ", ', /, ], or `.
+      # %ni   Indent level. The optional number n is used as a width specification to printf, as printf("%nd").
+      # %nn   Current line number (n used as with the indent level).
+      # %%    A literal percent sign.
 
-    :PROMPT_I => "#{name}> ",                                      # standard prompt
-    :PROMPT_C => "#{' ' * name.size}> ",           # multi-line statment prompt
-    :PROMPT_S => "#{' ' * (name.size - 1)}%l> ",   # multi-line string prompt
+      :PROMPT_I => "#{name}> ",                                      # standard prompt
+      :PROMPT_C => "#{' ' * name.size}> ",           # multi-line statment prompt
+      :PROMPT_S => "#{' ' * (name.size - 1)}%l> ",   # multi-line string prompt
 
-    :RETURN => "#{red}#{' ' * (name.size - 1)}=> %s#{clear}\n",  # return value formatting, see table below
-  }
-  IRB.conf[:PROMPT_MODE] = :CUSTOM
+      :RETURN => "#{red}#{' ' * (name.size - 1)}=> %s#{clear}\n",  # return value formatting, see table below
+    }
+    IRB.conf[:PROMPT_MODE] = :CUSTOM
+  end
+
+  if defined?(Pry)
+    Pry.config.prompt_name = name
+  end
 end
 
-setup_custom_irb_prompt('irb')
+setup_custom_repl_prompt('ruby')
--- railsrc.d/prompt.rb
+++ railsrc.d/prompt.rb
@@ -1,10 +1,10 @@
-def setup_rails_irb_prompt
+def setup_rails_repl_prompt
   dangerous_envs = %w(production smoke)
   rails_env = ENV['RAILS_ENV']
   rails_env = rails_env.upcase if dangerous_envs.include?(rails_env)
 
   prompt = defined?(DETECTED_RAILS_ROOT) ? File.basename(DETECTED_RAILS_ROOT) : 'rails'
-  setup_custom_irb_prompt(prompt + " #{rails_env}")
+  setup_custom_repl_prompt(prompt + " #{rails_env}")
 end
 
-setup_rails_irb_prompt
+setup_rails_repl_prompt
