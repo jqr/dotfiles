c9b5fa805783b2d0383478da6bdff43b7b226d4a
Elijah Miller <elijah.miller@gmail.com>
2018-02-13 15:01:27 -0500

    Automatically to_a on all SQL calls in Rails.

--- railsrc.d/sql.rb
+++ railsrc.d/sql.rb
@@ -1,7 +1,7 @@
 # From http://github.com/mislav/dotfiles/tree/master/railsrc
 
 def sql(query)
-  ActiveRecord::Base.connection.select_all(query)
+  ActiveRecord::Base.connection.select_all(query).to_a
 end
 
 def sql_bench(query, runs = 10)
