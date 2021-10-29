98c8f18e38aa5f62b52fb48913b57b0abf2525d5
Elijah Miller <elijah.miller@gmail.com>
2021-10-29 13:00:18 -0400

    x86 alias to force x86-64 architecture for a shell.

--- bash_profile.d/mac.sh
+++ bash_profile.d/mac.sh
@@ -6,3 +6,6 @@ alias ql='qlmanage -p 2>/dev/null'
 pman () {
     man -t "${1}" | open -f -a /Applications/Preview.app
 }
+
+# Load a new shell with x86 architecture
+alias x86='arch -x86_64 $SHELL --login'
