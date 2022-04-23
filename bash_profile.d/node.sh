96e723771e778a766df5a7d3fbb78249b23bef22
Elijah Miller <elijah.miller@gmail.com>
2022-04-23 15:39:55 -0400

    Use nvm.

--- bash_profile.d/node.sh
+++ bash_profile.d/node.sh
@@ -1,1 +1,5 @@
-export NODE_PATH="/usr/local/lib/node"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
