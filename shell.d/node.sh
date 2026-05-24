702db7c2ffd54835ec20dcc32dd531f634a39a6a
Elijah Miller <elijah.miller@gmail.com>
2026-05-23 23:36:01 -0400

    Lazy load nvm on first use for 265ms (~50%) savings on every shell.

--- shell.d/node.sh
+++ shell.d/node.sh
@@ -1,7 +1,16 @@
 export NVM_DIR="$HOME/.nvm"
-# shellcheck disable=SC1091 # paths depend on nvm installation
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# shellcheck disable=SC1091
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+__load_nvm() {
+  unset -f nvm node npm npx
+  # shellcheck disable=SC1091 # paths depend on nvm installation
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  # shellcheck disable=SC1091
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+}
+
+nvm()  { __load_nvm; nvm  "$@"; }
+node() { __load_nvm; node "$@"; }
+npm()  { __load_nvm; npm  "$@"; }
+npx()  { __load_nvm; npx  "$@"; }
 
 alias node-repl='rlwrap node-repl || node-repl'
