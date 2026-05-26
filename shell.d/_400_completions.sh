c667c3499999db8e83d6adc848e6218a8a7ba56a
Elijah Miller <elijah.miller@gmail.com>
2026-05-26 19:21:43 -0400

    Fancy kill on zsh.

--- shell.d/_400_completions.sh
+++ shell.d/_400_completions.sh
@@ -1,4 +1,8 @@
 if [ -n "$ZSH_VERSION" ]; then
   autoload -Uz compinit && compinit
   autoload -Uz bashcompinit && bashcompinit
+
+  zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+  zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+  zstyle ':completion:*:*:kill:*' menu yes select
 fi
